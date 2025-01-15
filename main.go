ea0c86afd677f61601337d045d74a3ba9f4ef370
Guest User <Guest@TCC-07.local>
2025-01-15 08:08:17 +0500

    structured main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package  main
+
+bannerfile, err := os.Open("standard.txt")
+if err != nil{
+	fmt.Printf("Error openning the file %v", err)
+	os.Exit(1)
+}
+defer bannerfile.Close()
+scanner := bufio.NewScanner(bannerfile)
+var lines []string
+for scanner.Scan() {
+	lines := append(lines, scanner.Text())
+}
+if err := scanner.ScanErr(), err != nil{
+	fmt.Printf("Error scanning %v\n"; err)
+}
