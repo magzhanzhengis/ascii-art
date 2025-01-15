ea1f37ef9d29583895bc5b8e068aef04acd18559
mzhengis <mzhengis@TCB-05.local>
2025-01-15 09:58:06 +0500

    debugging banner

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
-// package  main
+package main
 
-// bannerfile, err := os.Open("standard.txt")
-// if err != nil{
-// 	fmt.Printf("Error openning the file %v", err)
-// 	os.Exit(1)
-// }
-// defer bannerfile.Close()
-// scanner := bufio.NewScanner(bannerfile)
-// var lines []string
-// for scanner.Scan() {
-// 	lines := append(lines, scanner.Text())
-// }
-// if err := scanner.ScanErr(), err != nil{
-// 	fmt.Printf("Error scanning %v\n"; err)
-// }
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Load the banner data
+	banner, err := loadbanner()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	// Debug: Print the banner data
+	for char, ascii := range banner {
+		fmt.Printf("Character: %q\n", char)
+		for _, line := range ascii {
+			fmt.Println(line)
+		}
+		fmt.Println()
+	}
+}
